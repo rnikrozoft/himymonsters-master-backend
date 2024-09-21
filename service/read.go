177e64995283f93973b498eef4911638896ddb68
rnikrozoft <rnikrozoft.dev@gmail.com>
2024-09-21 11:31:40 +0700

    Add function to read data from Google Sheets

---
+++ service/read.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+)
+
+func ReadGoogleSheet(spreadsheetId, readRange string) [][]interface{} {
+	ctx := context.Background()
+	b, err := os.ReadFile("./config/himymonsters-bebd75d59c7a.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	config, err := google.JWTConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := config.Client(ctx)
+
+	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
+	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	}
+
+	return resp.Values
+}
